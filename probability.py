ce41b634bcf4189327e5c74231a0e9def22900ad
Aden King <adenking@live.com>
2018-12-27 21:33:09 +1000

    finished mini-project requires a bit of manually handling files

---
+++ probability.py
@@ -0,0 +1,19 @@
+'''
+https://www.reddit.com/r/dndnext/comments/9e52vk/300_ways_to_roll_for_stats_from_up_to_op/
+this is a calculator for dice rolls not on this list
+use anydice to get a csv chart with your desired roll and put it in a csv and then run this
+https://anydice.com/
+'''
+import csv
+total = 0
+with open('PBE.csv') as pbefile:
+    pbereader = csv.reader(pbefile)
+    pbedict = {rows[0]: rows[1] for rows in pbereader}
+
+with open('probability.csv') as csvfile:
+    probreader = csv.reader(csvfile, delimiter=",", quotechar='|')
+    for row in probreader:
+        total += float(pbedict[row[0]])*(float(row[1])/100)
+
+
+print(f'Total: {total*6/27}')
